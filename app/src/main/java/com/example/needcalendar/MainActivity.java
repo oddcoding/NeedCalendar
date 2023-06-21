36338f64ae76f1a85265ecd5c0e45cece0f08836
pos <pos0910@hs.ac.kr>
2023-06-21 17:08:42 +0900

    calendar

--- app/src/main/java/com/example/needcalendar/MainActivity.java
+++ app/src/main/java/com/example/needcalendar/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.needcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,23 +19,48 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
-{
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
 
+    Dialog dialog1;
+    Button button;
+    Context context;
+    AlertDialog alertDialog;
+
+    private RecyclerView mPostRecyclerView;
+    private checklist mAdapter;
+    private List<list> mDatas;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
         selectedDate = LocalDate.now();
         setMonthView();
+        context = this;
+        setContentView(R.layout.activity_main);
+        mPostRecyclerView = findViewById(R.id.list_rv);
+        button = findViewById(R.id.item_button);
+        mDatas = new ArrayList<>();
+
+        mDatas.add(new list("title1", "place1", "Memo1"));
+        mDatas.add(new list("title2", "place2", "Memo2"));
+        mDatas.add(new list("title3", "place3", "Memo3"));
+        mDatas.add(new list("title4", "place4", "Memo4"));
+        mDatas.add(new list("title5", "place5", "Memo5"));
+
+        mAdapter = new checklist(mDatas);
+        mPostRecyclerView.setAdapter(mAdapter);
+        mPostRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 
     private void initWidgets()
     {
