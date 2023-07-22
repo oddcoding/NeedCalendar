f0a051daeaf947bf13e1fc034638c3b903203c9a
Hyeyeong An <oddcoding64@gmail.com>
2023-07-22 15:25:44 +0900

    menu weekly MainActivity connect

---
+++ app/src/main/java/com/example/needcalendar/MenuViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.needcalendar;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.menu);
+    }
+}
