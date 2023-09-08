283ddba115d7d03dde60975ef4416eb8ff96ccb1
Hyeyeong An <oddcoding64@gmail.com>
2023-09-08 15:11:15 +0900

    is not okay(register and login)

--- app/src/main/java/com/example/needcalendar/MenuViewActivity.java
+++ app/src/main/java/com/example/needcalendar/MenuViewActivity.java
@@ -1,6 +1,9 @@
 package com.example.needcalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +15,16 @@ public class MenuViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.menu);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent8 = new Intent(getApplicationContext(), login.class);
+
+                startActivity(intent8);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/needcalendar/login.java
+++ app/src/main/java/com/example/needcalendar/login.java
@@ -1,6 +1,9 @@
 package com.example.needcalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,16 @@ public class login extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent10 = new Intent(getApplicationContext(), RegisterActivity.class);
+
+                startActivity(intent10);
+            }
+        });
     }
 }
